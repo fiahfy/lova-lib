4191c6f57dc5ac78a4e93ff42e4407009b7964c7
fiahfy <d.fiahfy@gmail.com>
2015-08-25 22:06:51 +0900

    fix

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -3,3 +3,20 @@
 /* Directives */
 
 var directives = angular.module('directives', []);
+
+//directives.directive('lazy', function($timeout) {
+//  return {
+//    restrict: 'C',
+//    link: function(scope, elm) {
+//      $timeout(function() {
+//        $(elm).lazyload({
+//          threshold: -300,
+//          effect: 'fadeIn',
+//          event: 'scroll',
+//          effectspeed: 1000,
+//          'skip_invisible': false
+//        });
+//      }, 0);
+//    }
+//  };
+//});
--- assets/js/filters.js
+++ assets/js/filters.js
@@ -5,7 +5,7 @@
 var filters = angular.module('filters', []);
 
 filters.filter('pad', function() {
-  return function (input, length, string) {
+  return function(input, length, string) {
     return ((new Array(length+1)).join(string) + input).slice(-length);
   };
 });
