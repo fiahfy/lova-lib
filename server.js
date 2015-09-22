8fa14a6913bc38cb980ee082987e943ea6ef096b
fiahfy <d.fiahfy@gmail.com>
2015-09-22 13:13:54 +0900

    test

--- server.js
+++ server.js
@@ -103,10 +103,10 @@ var SampleApp = function() {
         self.routes['/env'] = function(req, res) {
           var content = 'Version: ' + process.version + '\n<br/>\n' +
             'Env: {<br/>\n<pre>';
-          //  Add env entries.
-          for (var k in process.env) {
-            content += '   ' + k + ': ' + process.env[k] + '\n';
-          }
+          ////  Add env entries.
+          //for (var k in process.env) {
+          //  content += '   ' + k + ': ' + process.env[k] + '\n';
+          //}
           content += '}\n</pre><br/>\n';
           res.send('<html>\n' +
           '  <head><title>Node.js Process Env</title></head>\n' +
