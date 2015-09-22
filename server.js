87528113286325c663d45152c9b6e5b4f32169df
fiahfy <d.fiahfy@gmail.com>
2015-09-22 17:37:54 +0900

    Fix

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+'use strict';
+
 var koa = require('koa');
 var app = koa();
+
+var config = {
+  port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
+  ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+};
 
 app.use(function * () {
   this.body = 'OK';
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.listen(config.port, config.ip);
