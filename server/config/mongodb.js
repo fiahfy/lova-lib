f447a0a65d1753277408fa521b82ddacd0e5a475
fiahfy <d.fiahfy@gmail.com>
2015-09-27 18:54:02 +0900

    Use assets

---
+++ server/config/mongodb.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var config = {
+  host:   process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1',
+  port:   process.env.OPENSHIFT_MONGODB_DB_PORT || 27017,
+  user:   process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+  pass:   process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+  scheme: 'mongodb',
+  db:     'lova'
+};
+
+config.uri = (function(config) {
+  var uri = '';
+  if (config.user) {
+    uri += config.user;
+    if (config.pass) {
+      uri += `:${config.pass}`;
+    }
+    uri += '@';
+  }
+  uri += config.host;
+  if (config.port) {
+    uri += `:${config.port}`;
+  }
+  return `${config.scheme}://${uri}/${config.db}`;
+})(config);
+
+module.exports = config;
