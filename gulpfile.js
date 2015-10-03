8e71a092cc7186a5a4757808dfecd2c96304fa21
fiahfy <d.fiahfy@gmail.com>
2015-10-03 17:00:46 +0900

    Update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,23 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
-//var jsmin = require('gulp-jsmin');
-//var rename = require('gulp-rename');
-//
-var dest = '../dest';
-var src = '../src';
-var path = require('path');
-var relativeSrcPath = path.relative('.', src);
-//
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+
 var config = {
-  //minify: {
-  //  src: dest + '/js/*.js',
-  //  dest: dest + '/js/min'
-  //},
-  //
+  minify: {
+    src: 'client/assets/js/bundle.js',
+    dest: 'client/assets/js/'
+  },
   tsd: {
     optoins: {
       command: 'reinstall',
       config: 'tsd.json'
     }
   },
-
   tsc: {
-    src: [
-      'client/assets/js/**/*.ts'
-    ],
+    src: 'client/assets/js/**/*.ts',
     dest: 'client/assets/js/',
     options: {
       noImplicitAny : true,
@@ -40,7 +32,6 @@ var config = {
       module : 'commonjs'
     }
   },
-
   browserify: {
     options: {
       entries: [
@@ -48,15 +39,15 @@ var config = {
       ],
       debug: true
     },
-    dest: 'client/assets/js',
+    dest: 'client/assets/js/',
     output: {
       filename: 'bundle.js'
     }
   },
-
   watch: {
     ts: 'client/assets/js/**/*.ts',
-    js: ['client/assets/js/**/*.js', '!client/assets/js/bundle.js']
+    js: ['client/assets/js/**/*.js', '!client/assets/js/bundle.js'],
+    bundle: 'client/assets/js/bundle.js'
   }
 };
 
@@ -64,13 +55,13 @@ gulp.task('tsd', function (callback) {
   tsd(config.tsd.optoins, callback);
 });
 
-gulp.task('tsc', function(){
-  gulp.src(config.tsc.src)
+gulp.task('tsc', function() {
+  return gulp.src(config.tsc.src)
     .pipe(tsc(config.tsc.options))
     .pipe(gulp.dest(config.tsc.dest));
 });
 
-gulp.task('browserify', function(){
+gulp.task('browserify', function() {
   return browserify(config.browserify.options)
     .bundle()
     .pipe(source(config.browserify.output.filename))
@@ -80,21 +71,29 @@ gulp.task('browserify', function(){
     .pipe(gulp.dest(config.browserify.dest));
 });
 
-gulp.task('watch', ['tsc', 'browserify'], function () {
-  watch(config.watch.ts, function () {
+gulp.task('watch', ['build'], function() {
+  watch(config.watch.ts, function() {
     gulp.start(['tsc']);
   });
 
-  watch(config.watch.js, function () {
+  watch(config.watch.js, function() {
     gulp.start(['browserify']);
   });
+
+  watch(config.watch.bundle, function() {
+    gulp.start(['minify']);
+  });
 });
-//
-//gulp.task('minify', function () {
-//  gulp.src(config.js.src)
-//    .pipe(jsmin())
-//    .pipe(rename({suffix: '.min'}))
-//    .pipe(gulp.dest(config.js.dest));
-//});
-//
-//gulp.task('default', ['watch']);
+
+gulp.task('minify', function() {
+  return gulp.src(config.minify.src)
+    .pipe(jsmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(config.minify.dest));
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'tsd', 'tsc', 'browserify', 'minify', callback
+  );
+});
