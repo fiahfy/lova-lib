04bf11f4bd623cd1d7a76c2b5dfba0b489d4bed4
fiahfy <d.fiahfy@gmail.com>
2015-10-03 22:18:08 +0900

    Fix

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -407,6 +407,139 @@ var lova;
 'use strict';
 var lova;
 (function (lova) {
+    var ServantService = (function () {
+        function ServantService($http, $q) {
+            this.$http = $http;
+            this.$q = $q;
+            this.servants = [];
+        }
+        ServantService.prototype.load = function () {
+            var _this = this;
+            var deferrd = this.$q.defer();
+            if (this.servants.length) {
+                deferrd.resolve();
+                return deferrd.promise;
+            }
+            this.$http.get(ServantService.url)
+                .then(function (res) {
+                res.data.forEach(function (servant) {
+                    _this.servants.push(new lova.ServantModel(servant));
+                });
+                deferrd.resolve();
+            }, function () {
+                deferrd.reject();
+            });
+            return deferrd.promise;
+        };
+        ServantService.prototype.getServantWithId = function (id) {
+            var result = null;
+            this.servants.forEach(function (servant) {
+                if (servant.id == id) {
+                    result = servant;
+                }
+            });
+            return result;
+        };
+        ServantService.url = './api/servants/';
+        ServantService.$inject = [
+            '$http',
+            '$q'
+        ];
+        return ServantService;
+    })();
+    lova.ServantService = ServantService;
+    var DeckService = (function () {
+        function DeckService($window) {
+            this.$window = $window;
+            this.servants = [];
+            this.deck = new lova.DeckModel();
+        }
+        Object.defineProperty(DeckService.prototype, "url", {
+            get: function () {
+                var a = this.$window.document.createElement('a');
+                a.href = this.$window.location.href;
+                return a.protocol + '//'
+                    + a.hostname + (a.port ? ':' + a.port : a.port)
+                    + '/deck/' + this.deck.hash + '/';
+            },
+            enumerable: true,
+            configurable: true
+        });
+        DeckService.prototype.loadWithHash = function (hash) {
+            this.deck.hash = hash;
+            this.deck.updateServants(this.servants);
+        };
+        DeckService.prototype.setServant = function (index, servantId) {
+            this.deck.servantIds[index] = servantId;
+            this.deck.updateServants(this.servants);
+        };
+        DeckService.prototype.unsetServant = function (index) {
+            this.setServant(index, undefined);
+        };
+        DeckService.$inject = [
+            '$window'
+        ];
+        return DeckService;
+    })();
+    lova.DeckService = DeckService;
+    var PrizeService = (function () {
+        function PrizeService($http, $q) {
+            this.$http = $http;
+            this.$q = $q;
+            this.prizes = [];
+        }
+        PrizeService.prototype.load = function () {
+            var _this = this;
+            var deferrd = this.$q.defer();
+            if (this.prizes.length) {
+                deferrd.resolve();
+                return deferrd.promise;
+            }
+            this.$http.get(PrizeService.url)
+                .then(function (res) {
+                res.data.forEach(function (servant) {
+                    _this.prizes.push(new lova.PrizeModel(servant));
+                });
+                deferrd.resolve();
+            }, function () {
+                deferrd.reject();
+            });
+            return deferrd.promise;
+        };
+        PrizeService.url = './api/prizes/';
+        PrizeService.$inject = [
+            '$http',
+            '$q'
+        ];
+        return PrizeService;
+    })();
+    lova.PrizeService = PrizeService;
+    var ScrollService = (function () {
+        function ScrollService($location, $window) {
+            var _this = this;
+            this.$location = $location;
+            this.$window = $window;
+            this.positions = {};
+            angular.element($window).on('scroll', function () {
+                _this.positions[_this.$location.path()] = angular.element($window).scrollTop();
+            });
+        }
+        ScrollService.prototype.restore = function () {
+            var top = this.positions[this.$location.path()] || 0;
+            angular.element(this.$window).scrollTop(top);
+        };
+        ScrollService.$inject = [
+            '$location',
+            '$window'
+        ];
+        return ScrollService;
+    })();
+    lova.ScrollService = ScrollService;
+})(lova || (lova = {}));
+/// <reference path="_all.ts" />
+'use strict';
+var lova;
+(function (lova) {
     var ServantModel = (function () {
         function ServantModel(obj) {
             this.id = obj.id;
@@ -562,6 +695,13 @@ var lova;
     })();
     lova.PrizeModel = PrizeModel;
 })(lova || (lova = {}));
+/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="controllers.ts" />
+/// <reference path="directives.ts" />
+/// <reference path="filters.ts" />
+/// <reference path="services.ts" />
+/// <reference path="models.ts" />
+/// <reference path="app.ts" />
 /// <reference path="_all.ts" />
 'use strict';
 var lova;
@@ -651,145 +791,5 @@ var lova;
     app.service('DeckService', lova.DeckService);
     app.service('PrizeService', lova.PrizeService);
     app.service('ScrollService', lova.ScrollService);
-})(lova || (lova = {}));
-/// <reference path="../../typings/tsd.d.ts" />
-/// <reference path="controllers.ts" />
-/// <reference path="directives.ts" />
-/// <reference path="filters.ts" />
-/// <reference path="services.ts" />
-/// <reference path="models.ts" />
-/// <reference path="app.ts" />
-/// <reference path="_all.ts" />
-'use strict';
-var lova;
-(function (lova) {
-    var ServantService = (function () {
-        function ServantService($http, $q) {
-            this.$http = $http;
-            this.$q = $q;
-            this.servants = [];
-        }
-        ServantService.prototype.load = function () {
-            var _this = this;
-            var deferrd = this.$q.defer();
-            if (this.servants.length) {
-                deferrd.resolve();
-                return deferrd.promise;
-            }
-            this.$http.get(ServantService.url)
-                .then(function (res) {
-                res.data.forEach(function (servant) {
-                    _this.servants.push(new lova.ServantModel(servant));
-                });
-                deferrd.resolve();
-            }, function () {
-                deferrd.reject();
-            });
-            return deferrd.promise;
-        };
-        ServantService.prototype.getServantWithId = function (id) {
-            var result = null;
-            this.servants.forEach(function (servant) {
-                if (servant.id == id) {
-                    result = servant;
-                }
-            });
-            return result;
-        };
-        ServantService.url = './api/servants/';
-        ServantService.$inject = [
-            '$http',
-            '$q'
-        ];
-        return ServantService;
-    })();
-    lova.ServantService = ServantService;
-    var DeckService = (function () {
-        function DeckService($window) {
-            this.$window = $window;
-            this.servants = [];
-            this.deck = new lova.DeckModel();
-        }
-        Object.defineProperty(DeckService.prototype, "url", {
-            get: function () {
-                var a = this.$window.document.createElement('a');
-                a.href = this.$window.location.href;
-                return a.protocol + '//'
-                    + a.hostname + (a.port ? ':' + a.port : a.port)
-                    + '/deck/' + this.deck.hash + '/';
-            },
-            enumerable: true,
-            configurable: true
-        });
-        DeckService.prototype.loadWithHash = function (hash) {
-            this.deck.hash = hash;
-            this.deck.updateServants(this.servants);
-        };
-        DeckService.prototype.setServant = function (index, servantId) {
-            this.deck.servantIds[index] = servantId;
-            this.deck.updateServants(this.servants);
-        };
-        DeckService.prototype.unsetServant = function (index) {
-            this.setServant(index, undefined);
-        };
-        DeckService.$inject = [
-            '$window'
-        ];
-        return DeckService;
-    })();
-    lova.DeckService = DeckService;
-    var PrizeService = (function () {
-        function PrizeService($http, $q) {
-            this.$http = $http;
-            this.$q = $q;
-            this.prizes = [];
-        }
-        PrizeService.prototype.load = function () {
-            var _this = this;
-            var deferrd = this.$q.defer();
-            if (this.prizes.length) {
-                deferrd.resolve();
-                return deferrd.promise;
-            }
-            this.$http.get(PrizeService.url)
-                .then(function (res) {
-                res.data.forEach(function (servant) {
-                    _this.prizes.push(new lova.PrizeModel(servant));
-                });
-                deferrd.resolve();
-            }, function () {
-                deferrd.reject();
-            });
-            return deferrd.promise;
-        };
-        PrizeService.url = './api/prizes/';
-        PrizeService.$inject = [
-            '$http',
-            '$q'
-        ];
-        return PrizeService;
-    })();
-    lova.PrizeService = PrizeService;
-    var ScrollService = (function () {
-        function ScrollService($location, $window) {
-            var _this = this;
-            this.$location = $location;
-            this.$window = $window;
-            this.positions = {};
-            angular.element($window).on('scroll', function () {
-                _this.positions[_this.$location.path()] = angular.element($window).scrollTop();
-            });
-        }
-        ScrollService.prototype.restore = function () {
-            var top = this.positions[this.$location.path()] || 0;
-            angular.element(this.$window).scrollTop(top);
-        };
-        ScrollService.$inject = [
-            '$location',
-            '$window'
-        ];
-        return ScrollService;
-    })();
-    lova.ScrollService = ScrollService;
 })(lova || (lova = {}));
 //# sourceMappingURL=main.js.map
