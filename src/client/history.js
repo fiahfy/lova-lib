a35ebcb3e62bd8c9a636ed2a1db653ba054d723c
fiahfy <d.fiahfy@gmail.com>
2015-12-27 18:32:55 +0900

    Update scroll behavior

--- src/client/history.js
+++ src/client/history.js
@@ -1,6 +1,7 @@
 import {createHistory, createMemoryHistory, useQueries} from 'history'
+import {useSimpleScroll} from 'scroll-behavior'
 import ExecutionEnvironment from 'fbjs/lib/ExecutionEnvironment'
 
-export default useQueries(
+export default useSimpleScroll(useQueries(
   ExecutionEnvironment.canUseDOM ? createHistory : createMemoryHistory
-)()
+))()
